05a0d535a68e4422be130aec5d2383f1647b7d9d
aryannema <aaryan.techy@gmail.com>
2026-02-15 19:37:15 +0530

    added initial files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from "./assets/react.svg";
+
+export default function App() {
+  return (
+    <main>
+      <img src={reactLogo} width="40" alt="React Logo" />
+      <h1>Fun facts about React!</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+}
