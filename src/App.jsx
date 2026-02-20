7cc0a4172d0e59225ccb0250f2adbb42fdb777cd
aryannema <aaryan.techy@gmail.com>
2026-02-20 16:13:33 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import reactLogo from "./assets/react.svg";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import "./App.css";
 
-export default function App() {
+function App() {
   return (
-    <main>
-      <img src={reactLogo} width="40" alt="React Logo" />
-      <h1>Fun facts about React!</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on Github</li>
-        <li>Is maintained by Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-    </main>
+    <>
+      <Navbar />
+      <Main />
+    </>
   );
 }
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import reactLogo from "../assets/react-logo.png";
+
+function Navbar() {
+  return (
+    <header>
+      <nav>
+        <img src={reactLogo} alt="React logo" />
+        <span>ReactFacts</span>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
